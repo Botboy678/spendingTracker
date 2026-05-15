02a0cc1673dc7f6aa7744d0fde7f3e25aaaaabdf
August <Emmanueldizulu@gmail.com>
2026-05-15 00:08:56 -0500

    Changed title font and add entry btn

--- spending-tracker-frontend/src/App.tsx
+++ spending-tracker-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Modal from "./components/Modals/Modal";
 import { useState } from "react";
 import './css/App.css'
 import { AnimatePresence, motion } from "framer-motion";
+import { ExpandIcon } from "./assets/icons/Icons";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -12,7 +13,13 @@ function App() {
       <div className="bg">
         <div className="navbar-wrapper">
           <Navbar userName="Manny" />
-          <button id="view-table-btn" onClick={() => setShowModal(true)}>Table</button>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="btn btn-outline-success rounded-15 mt-3 border-0"
+            id="view-table-btn" onClick={() => setShowModal(true)}>
+            <ExpandIcon size={40} color="#6edf88" />
+          </motion.button>
           <AnimatePresence>
             {showModal && (
               <Modal onClose={() => setShowModal(false)} />
