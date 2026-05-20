87c316b8ba7703174bdeb2bf60b5fbcfbe1ee381
August <Emmanueldizulu@gmail.com>
2026-05-19 21:46:47 -0500

    added routing for pages

---
+++ spending-tracker-frontend/src/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+        </div>
+    )
+}
--- spending-tracker-frontend/src/main.tsx
+++ spending-tracker-frontend/src/main.tsx
@@ -4,9 +4,19 @@ import App from "./App.tsx";
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import { CursorTrail } from "./components/Cursor.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./Login.tsx";
+import { NotFoundPage } from "./NotfoundPage.tsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <NotFoundPage /> }
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CursorTrail />
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
